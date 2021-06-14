2d6deba20a69b4f0d0aa2c941aa78fe9ab726c7b
Pham Hong Duc <iamphduc@gmail.com>
2021-06-14 16:55:24 +0700

    refactor constant folder

--- src/constants/dateConst.js
+++
@@ -1,9 +0,0 @@
-module.exports = Object.freeze({
-    'mon' : 'Thứ 2',
-    'tue' : 'Thứ 3',
-    'wed' : 'Thứ 4',
-    'thu' : 'Thứ 5',
-    'fri' : 'Thứ 6',
-    'sat' : 'Thứ 7',
-    'sun' : 'CN',
-})
