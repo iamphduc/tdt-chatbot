254f6d53da3eaf5415f3dcfebd8c67430f4875ee
Pham Hong Duc <iamphduc@gmail.com>
2021-06-16 23:11:22 +0700

    change school constants

---
+++ src/constants/school.const.js
@@ -0,0 +1,81 @@
+module.exports = Object.freeze({
+    currSemester : '109',
+    scoreSemester : {
+        'hk2-20-21': 'Diem202',
+        'hk1-20-21': 'Diem201',
+        'hk3-19-20': 'Diem20193',
+        'hk2-19-20': 'Diem20192',
+        'hk1-19-20': 'Diem20191',
+    }
+});
+
+/*
+	<option value="113">HK hè/2021-2022</option>
+	<option value="115">HK dự thính 2/2021-2022</option>
+	<option value="112">HK2/2021-2022</option>
+	<option value="114">HK dự thính 1/2021-2022</option>
+	<option value="110">HK1/2021-2022</option>
+	<option selected="selected" value="109">HK Hè/2020-2021</option>
+	<option value="108">HK Dự thính 2/2020-2021</option>
+	<option value="106">HK2/2020-2021</option>
+	<option value="107">HK Dự thính 1/2020-2021</option>
+	<option value="105">HK1/2020-2021</option>
+	<option value="102">HK hè/2019-2020</option>
+	<option value="104">HK dự thính 2/2019-2020</option>
+	<option value="101">HK2/2019-2020</option>
+	<option value="103">HK dự thính 1/2019-2020</option>
+	<option value="99">HK1/2019-2020</option>
+	<option value="97">HK hè/2018-2019</option>
+	<option value="98">HK dự thính 2/2018-2019</option>
+	<option value="96">HK2/2018-2019</option>
+	<option value="95">HK dự thính 1/2018-2019</option>
+	<option value="94">HK1/2018-2019</option>
+	<option value="93">HK hè/2017-2018</option>
+	<option value="92">HK dự thính 2/2017-2018</option>
+	<option value="91">HK2/2017-2018</option>
+	<option value="90">HK dự thính 1/2017-2018</option>
+	<option value="89">HK1/2017-2018</option>
+	<option value="88">HK hè/2016-2017</option>
+	<option value="87">HK dự thính 2/2016-2017</option>
+	<option value="86">HK2/2016-2017</option>
+	<option value="85">HK dự thính 1/2016-2017</option>
+	<option value="84">HK1/2016-2017</option>
+	<option value="83">HK hè/2015-2016</option>
+	<option value="82">HK dự thính 2/2015-2016</option>
+	<option value="81">HK2/2015-2016</option>
+	<option value="80">HK dự thính 1/2015-2016</option>
+	<option value="79">HK1/2015-2016</option>
+	<option value="78">HK hè/2014-2015</option>
+	<option value="77">HK dự thính 2/2014-2015</option>
+	<option value="76">HK2/2014-2015</option>
+	<option value="75">HK dự thính 1/2014-2015</option>
+	<option value="74">HK1/2014-2015</option>
+	<option value="73">HK hè/2013-2014</option>
+	<option value="72">HK dự thính 2/2013-2014</option>
+	<option value="71">HK2/2013-2014</option>
+	<option value="70">HK dự thính 1/2013-2014</option>
+	<option value="69">HK1/2013-2014</option>
+	<option value="68">HK hè/2012-2013</option>
+	<option value="67">HK dự thính 2/2012-2013</option>
+	<option value="65">HK2/2012-2013</option>
+	<option value="66">HK2/2012-2013</option>
+	<option value="64">HK dự thính 1/2012-2013</option>
+	<option value="63">HK1/2012-2013</option>
+	<option value="62">HK hè/2011-2012</option>
+	<option value="61">Học kỳ dự thính 2011-2012</option>
+	<option value="12">HK2/2011-2012</option>
+	<option value="11">HK1/2011-2012</option>
+	<option value="1">HK hè/2010-2011</option>
+	<option value="18">AV</option>
+	<option value="14">Học kỳ tạm</option>
+	<option value="10">HK2/2010-2011</option>
+	<option value="8">HK1/2010-2011</option>
+	<option value="7">HK hè/2009-2010</option>
+	<option value="6">HK2/2009-2010</option>
+	<option value="5">HK1/2009-2010</option>
+	<option value="4">HK hè/2008-2009</option>
+	<option value="3">HK2/2008-2009</option>
+	<option value="17">Nam2011vaHK12012</option>
+	<option value="2">HK1/2008-2009</option>
+	<option value="9">Bảo lưu</option>
+*/
