fd5fc66047abaae11398f56fa3c185de9acf1b3f
Pham Hong Duc <iamphduc@gmail.com>
2021-09-17 02:41:10 +0700

    Remove used headers

--- src/modules/School.js
+++ src/modules/School.js
@@ -17,44 +17,36 @@ class School {
 
   async login(user, pass) {
     try {
-      console.log('\n========== REQUEST ==========');
+      console.log('========== REQUEST ==========');
 
       console.time('Login');
       const { url } = await rp({
         method: 'POST',
         uri: URL,
-        form: {
-          user,
-          pass,
-        },
-        headers: {
-          'User-Agent':
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36',
-          Accept: '*/*',
-          'Accept-Encoding': 'gzip, deflate, br',
-          'Accept-Language': 'en-US,en;q=0.9',
-          Connection: 'keep-alive',
-          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        },
+        formData: { user, pass },
         json: true,
       });
       console.timeEnd('Login');
 
       // setting cookie manually is faster than authentication cookie
       const token = url.slice(url.indexOf('Token=') + 6);
-      const date = new Date(
-        86400000 + 1000 * 60 * 30 + +new Date()
-      ).toLocaleString();
-
-      this.jar.setCookie(
-        'AUTH_COOKIE=' + token + '|' + date + '; path=/',
-        'http://sso.tdt.edu.vn/Authenticate.aspx'
-      );
+      setAuthCookie(this.jar, token);
     } catch (err) {
       console.log('School - login error:' + err);
       return err;
     }
   }
+}
+
+function setAuthCookie(jar, token) {
+  const date = new Date(
+    86400000 + 1000 * 60 * 30 + +new Date()
+  ).toLocaleString();
+
+  jar.setCookie(
+    'AUTH_COOKIE=' + token + '|' + date + '; path=/',
+    'http://sso.tdt.edu.vn/Authenticate.aspx'
+  );
 }
 
 module.exports = School;
