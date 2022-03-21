5f617edfed9f1010d8f898506db74226b81efc9c
Pham Hong Duc <iamphduc@gmail.com>
2022-03-21 17:48:14 +0700

    fix: missing logger in error route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,15 +4,22 @@ import { apiRouter } from "./api.router";
 import { webRouter } from "./web.router";
 import { webhookRouter } from "./webhook.router";
 
+import logger from "../configs/logger";
+
 export const route = (app: Application) => {
   app.use("/", webRouter);
   app.use("/api", apiRouter);
   app.use("/webhook", webhookRouter);
 
   // Error - 404
-  app.use((req: Request, res: Response) => res.sendStatus(404));
+  app.use((req: Request, res: Response) => {
+    res.sendStatus(404);
+  });
 
   // Error - 500
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  app.use((err: any, req: Request, res: Response, next: NextFunction) => res.sendStatus(500));
+  app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err);
+    res.sendStatus(500);
+  });
 };
